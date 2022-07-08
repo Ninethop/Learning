8484a5a7a6941d08a0c72151e0d17ac48e46c295
Azrael <108619845+Ninethop@users.noreply.github.com>
2022-07-08 10:38:34 -0400

    new main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#pragma once
+//#include "snake.h"  //  Uncomment for snake
+
+int main()
+{
+  return 0;
+}
+
+// Uncomment below for snake
+/*int main()
+{
+	Snake demo;
+	if (demo.Construct(480, 360, 1, 1, false, true))
+		demo.Start();
+	return 0;
+}*/
